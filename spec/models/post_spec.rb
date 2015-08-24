16cc2c628e9048e4edee9b2f3000beaa44df7bfd
Ben Neely <benjaminneely@u.boisestate.edu>
2015-08-24 16:19:25 -0600

    Voting

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Post, type: :model do
   it { should have_many(:labels).through(:labelings) }
 
   it { should have_many(:comments) }
+  it { should have_many(:votes) }
   it { should belong_to(:topic) }
   it { should belong_to(:user) }
   it { should validate_presence_of(:title) }
@@ -27,6 +28,31 @@ RSpec.describe Post, type: :model do
 
     it "should respond to body" do
       expect(post).to respond_to(:body)
+    end
+  end
+
+  describe "voting" do
+    before do
+      3.times { post.votes.create!(value: 1) }
+      2.times { post.votes.create!(value: -1) }
+    end
+
+    describe "#up_votes" do
+      it "counts the number of votes with value = 1" do
+        expect( post.up_votes ).to eq(3)
+      end
+    end
+
+    describe "#down_votes" do
+      it "counts the number of votes with value = -1" do
+        expect( post.down_votes ).to eq(2)
+      end
+    end
+
+    describe "#points" do
+      it "returns the sum of all down and up votes" do
+        expect( post.points ).to eq(1) # 3 - 2
+      end
     end
   end
 end
