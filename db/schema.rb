c497d4bd66414cc62dc92a1f71c801651cbb6abc
robmadden <rob.madden@bloc.io>
2015-09-02 17:49:20 -0700

    Labels Assignment

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150903150656) do
+ActiveRecord::Schema.define(version: 20150915151808) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -61,12 +61,25 @@ ActiveRecord::Schema.define(version: 20150903150656) do
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
+  create_table "ratings", force: :cascade do |t|
+    t.integer  "severity"
+    t.integer  "topic_id"
+    t.integer  "post_id"
+    t.integer  "rateable_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "ratings", ["post_id"], name: "index_ratings_on_post_id"
+  add_index "ratings", ["topic_id"], name: "index_ratings_on_topic_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+    t.integer  "rating_id"
   end
 
   create_table "users", force: :cascade do |t|
